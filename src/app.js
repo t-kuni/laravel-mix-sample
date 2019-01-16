1e1cfa1f8081369ef278e5428b776259e2701f36
kunita <kunita@waltsinc.jp>
2019-01-16 12:29:09 +0900

    "Initial commit"

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+import VueComponent from './js/vue-component';
+
+new Vue({
+    el: '#app',
+    components: { VueComponent },
+    template: '<vue-component></vue-component>'
+});
---
+++ src/js/vue-component.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="root">
+        {{ body }}
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                body: 'I am a Vue Component.'
+            }
+        }
+    }
+</script>
+
+<style type="scss" scoped>
+    .root {
+        background: grey;
+    }
+</style>
